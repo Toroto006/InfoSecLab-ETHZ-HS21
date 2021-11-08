11ad1103c6976300e62ba44590f0866d3e6550bc
toroto006 <tobiaso@student.ethz.ch>
2021-11-08 20:00:57 +0100

    Deleted wrong files

--- Module4/test_setup.py
+++
@@ -1,8 +0,0 @@
-import test_manager
-import test_peripheral
-import test_string_parser
-
-def runTests():
-    test_manager.doHealtchCheck()
-    test_peripheral.doHealtchCheck()
-    test_string_parser.doHealtchCheck()
