9a91b996eca6ccd29edc943803453d8c1b66a5aa
Tobias <tobiaso@student.eth.ch>
2021-11-10 18:04:45 +0100

    flag-2-3 gotten?

--- Module4/Task2/submit-2.py
+++ Module4/Task2/submit-2.py
@@ -5,6 +5,7 @@ import signal
 from time import sleep
 import code
 import select
+import time
 import requests
 from requests.exceptions import ConnectionError
 
@@ -36,10 +37,19 @@ def readuntil(w, s, name):
     return s
 
 def injectHello(P):
+    # flag2-3
     readuntil(P, "Store takes some time", "PwaitS")
-    msg = '<start_messages><mes cd="hello"></mes></start_messages>'
+    # 1636560351705$00
+    ts = str(int(time.time()*1000))+"$00"
+    assert len(ts) == 16
+    msg = f'hello$0000000000{ts}0389289a839f8104a19535a57c60be21c2da9312df51308e0cd363ba82885b51111111111111111111111111111111112880ccda64f655478e053e178a2b4caf3441570c37c51e5eddeb4fd3f5a15d94653de7101e9c59229b666124029fc7d2dbf080223cc1e27635e9ea1f374e44bf86208d23fe97661d97ed63d5b1585afed679e7d2855f55d403e71c50ebe0390207d926be16293d3d2aa55e6ea6d9f013'
+    assert len(msg) == 352
+    headers = {
+        'Content-Type': 'application/xml',
+        'Content-Length': '352'
+        }
     try:
-        rep = requests.post(f"{peripheral_uri}/AAAA")
+        rep = requests.post(f"{peripheral_uri}/hello", data=msg, headers=headers)
         if rep.status_code == 200:
             print(f"P req returned {rep.text}")
         else:
--- Module4/Task2/test.py
+++ Module4/Task2/test.py
@@ -1,14 +1,31 @@
-m = "admin$00000000001636560351700$003575c066c711913e0b95b65aa1a9810c4726af722fc9a27974845a36ffb5aa4011111111111111111111111111111111af4a445d5bfee31c1fba656aefb089f112117264ee42ae55001f1d1a3bc561787439810809ab69fa140077db71232044813b626b53623b6d1a2fbc22f65f12eadfb7a4bc563719c11e063b744f5f91f9c412272ce5af58fbeace9d7862a3c1de8cbb695293a6fe76bf77e9493ad8eab1"
-m = "hello$00000000001636560351705$000389289a839f8104a19535a57c60be21c2da9312df51308e0cd363ba82885b51111111111111111111111111111111112880ccda64f655478e053e178a2b4caf3441570c37c51e5eddeb4fd3f5a15d94653de7101e9c59229b666124029fc7d2dbf080223cc1e27635e9ea1f374e44bf86208d23fe97661d97ed63d5b1585afed679e7d2855f55d403e71c50ebe0390207d926be16293d3d2aa55e6ea6d9f013"
-contt = m[:16]
-ts = m[16:2*16]
-print(f"content type: {contt}")
-print(f"timestamp: {ts}")
-mac = m[2*16:2*16+64]
-print(f"MAC: {mac}")
-hexenc = m[2*16+64:2*16+64+256]
-print(f"AES CBC: {hexenc}")
-iv = hexenc[:32]
-hexenc = hexenc[32:]
-print(f"IV: {iv}")
-print(hexenc)
+import glob
+import os
+import time
+
+def splitM(m):
+    contt = m[:16]
+    ts = m[16:2*16]
+    print(f"content type: {contt}")
+    print(f"timestamp: {ts}")
+    mac = m[2*16:2*16+64]
+    print(f"MAC: {mac}")
+    hexenc = m[2*16+64:2*16+64+256]
+    assert len(hexenc) == 256
+    #print(f"AES CBC len: {len(hexenc)}")
+    iv = hexenc[:32]
+    hexenc = hexenc[32:]
+    print(f"IV: {iv}")
+    print(f"hexenc without IV: {hexenc}\n")
+
+for filename in glob.glob(os.path.join("/home/isl/t2/share/Module4/", 'hello*.out')):
+    print(filename)
+    with open(os.path.join(os.getcwd(), filename), 'r') as f: # open in readonly mode
+        hexstream = f.readline().rstrip()[132:] # 132 is the garbage before
+        s = bytes.fromhex(hexstream).decode().rstrip().split('\n')
+        #print(f"{s[-1]}\n")
+        splitM(s[-1])
+
+ts = str(int(time.time()*1000))+"$00"
+assert len(ts) == 16
+msg = f'hello$0000000000{ts}0389289a839f8104a19535a57c60be21c2da9312df51308e0cd363ba82885b51111111111111111111111111111111112880ccda64f655478e053e178a2b4caf3441570c37c51e5eddeb4fd3f5a15d94653de7101e9c59229b666124029fc7d2dbf080223cc1e27635e9ea1f374e44bf86208d23fe97661d97ed63d5b1585afed679e7d2855f55d403e71c50ebe0390207d926be16293d3d2aa55e6ea6d9f013'
+assert len(msg) == 352
