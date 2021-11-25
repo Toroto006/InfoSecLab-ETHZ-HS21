ad97155df19b59a2cdd017ae6376b3a16a8f4dc0
toroto006 <tobiaso@student.ethz.ch>
2021-11-25 16:28:13 +0100

    Changes to get NOASLR working for the 5a

---
+++ Module5/run_all.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+unzip 18-951-566_mod5_submit.zip ./
+time python3 exploit1b.py NOASLR
+time python3 exploit2a.py NOASLR
+time python3 exploit2b.py NOASLR
+time python3 exploit3a.py NOASLR
+time python3 exploit3b.py
+time python3 exploit4a.py NOASLR
+time python3 exploit4b.py NOASLR
+time python3 exploit4c.py NOASLR
+time python3 exploit5a.py NOASLR
+time python3 exploit5b.py NOASLR
+time python3 exploit6a.py NOASLR
+time python3 exploit6b.py
